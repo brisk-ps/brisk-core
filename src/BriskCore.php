7d5dbdad662f086c1ab268c2d69408a696253c6f
Mohammed Fouad Ouda <mhmdowda@gmail.com>
2022-09-05 17:55:11 +0300

    Structuring

---
+++ src/BriskCore.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace BriskPs\BriskCore;
+
+class BriskCore{
+
+}
--- src/BriskCoreServiceProvider.php
+++ src/BriskCoreServiceProvider.php
@@ -7,10 +7,24 @@ use Illuminate\Support\ServiceProvider;
 class BriskCoreServiceProvider extends ServiceProvider{
 
     public function boot(){
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/../config/config.php' => config_path('brisk-core.php'),
+            ], 'config');
 
+            // Publishing assets.
+            /*$this->publishes([
+                __DIR__.'/../resources/assets' => public_path('vendor/brisk'),
+            ], 'assets');*/
+        }
     }
 
     public function register(){
-        
+        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'brisk-core');
+
+        // Register the main class to use with the facade
+        $this->app->singleton('brisk-core', function () {
+            return new BriskCore;
+        });
     }
 }
